95be7d6dc0b7e8f23e53a7476f25f880d4daa47f
Nugget <the.noodle.box.games@gmail.com>
2025-03-13 06:26:50 -0700

    Added bullets
    - bullets float across screen (will later make the bullets come out of the player character and connect with the enemy sprite)

--- RPG-Game/main.cpp
+++ RPG-Game/main.cpp
@@ -1,17 +1,73 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+#include <math.h>
+
+void NormalizeVector(sf::Vector2f vector)
+{
+	std::sqrt(vector.x * vector.x + vector.y * vector.y);
+}
 
 int main()
 {
-	// INITIALIZE
+	//Top ten tomar moments
+
+	std::vector<std::string> topTenTomarMoments;
+	topTenTomarMoments.reserve(10);
+
+	topTenTomarMoments.push_back("Final Fantasy XV 1");
+	topTenTomarMoments.push_back("Final Fantasy XV 2");
+
+	std::cout << topTenTomarMoments[0] << std::endl; // FFXV
+	std::cout << topTenTomarMoments[1] << std::endl; // FFXV
+
+	std::cout << "Count" << topTenTomarMoments.size() << std::endl; // 1
+	std::cout << "Capacity" << topTenTomarMoments.capacity() << std::endl; // 4
+
+
+	// INITIALIZE----------------------------------------------
 
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 8;
-	sf::RenderWindow window(sf::VideoMode(800, 600), "RPG Game", sf::Style::Default, settings);
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "RPG Game", sf::Style::Default, settings);
 
-	// INITIALIZE
+	// INITIALIZE------------------------------------------------
 
-	// Load
+	sf::RectangleShape bullet(sf::Vector2f(50, 25));
+	bullet.setPosition(sf::Vector2f(50, 50));
+	
+
+	// Load-------------------------------------------------------------
+
+	// Skeleton ---------------------------------------------------------
+	sf::Texture skeletonTexture;
+	sf::Sprite skeletonSprite;
+
+	if (skeletonTexture.loadFromFile("Assets/Skeleton/Textures/spritesheet.png"))
+	{
+		std::cout << "Enemy image loaded :D" << std::endl;
+		skeletonSprite.setTexture(skeletonTexture);
+		skeletonSprite.setPosition(sf::Vector2f(400, 100));
+
+		// starting point, size of wanted sprite
+		// x, y, Width, Height
+		int Xindex = 2;
+		int Yindex = 3;
+
+		skeletonSprite.setTextureRect(sf::IntRect(Xindex * 64, Yindex * 64, 64, 64));
+		skeletonSprite.scale(sf::Vector2f(3, 3));
+
+	}
+	else
+	{
+		std::cout << "Skeleton images failed to load ;-;" << std::endl;
+	}
+	// Skeleton ---------------------------------------------------------
+
+
+
+
+	// Player ---------------------------------------------------------
 	sf::Texture playerTexture;
 	sf::Sprite playerSprite;
 
@@ -26,13 +82,27 @@ int main()
 		int Yindex = 0;
 
 		playerSprite.setTextureRect(sf::IntRect(Xindex * 64, Yindex * 64, 64, 64));
+		playerSprite.scale(sf::Vector2f(3, 3));
+		playerSprite.setPosition(sf::Vector2f(1650, 800));
 	}
 	else
 	{
 		std::cout << "PLayer images failed to load :(" << std::endl;
 	}
+	// Player ---------------------------------------------------------
+
+
+
 
 	// Load
+
+	// calculate the direction of the bullet ----------------------------
+	
+	sf::Vector2f direction = skeletonSprite.getPosition() - bullet.getPosition();
+	
+
+	// calculate the direction of the bullet ----------------------------
+
 
 	// main game loop
 	while (window.isOpen())
@@ -46,6 +116,8 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
+		bullet.setPosition(bullet.getPosition() + direction * 0.0001f);
 
 		sf::Vector2f position = playerSprite.getPosition();
 
@@ -64,9 +136,14 @@ int main()
 
 		// ---------------UPDATE-------------
 
+
+
+
 		// ----------------DRAW--------------
 		window.clear(sf::Color::Black);
+		window.draw(skeletonSprite);
 		window.draw(playerSprite);
+		window.draw(bullet);
 		window.display();
 		// ----------------DRAW--------------
 	}
